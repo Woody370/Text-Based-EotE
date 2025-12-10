5c04aa90cfc3a4f5e76dd6a8b3539bfac41623bb
Woody370 <pwoodstrup@gmail.com>
2025-12-10 10:38:29 -0600

    Initial Commit: start of text-based EotE project

---
+++ character_sheet.py
@@ -0,0 +1,14 @@
+class Character:
+    def __init__(self, name, species, career, specialization, brawn, agility, intellect, cunning, willpower, presence):
+        self.name = name
+        self.species = species
+        self.career = career
+        self.specialization = specialization
+        self.brawn = brawn
+        self.agility = agility
+        self.intellect = intellect
+        self.cunning = cunning
+        self.willpower = willpower
+        self.presence = presence
+    
+    def create_character():
---
+++ main.py
@@ -0,0 +1,7 @@
+from character_sheet import create_character
+
+def main():
+    character = create_character()
+
+if __name__ == "__main__":
+    main()
